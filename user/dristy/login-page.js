21f1a3a0c0be18f394560c72d337a34a07980fd9
iptv-world <107808431+iptv-world@users.noreply.github.com>
2022-06-30 13:44:20 +0600

    Add files via upload

---
+++ user/dristy/login-page.js
@@ -0,0 +1,15 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "dristy" && password === "tusty") {
+        window.location.href="https://iptv-world.github.io/app/";
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
