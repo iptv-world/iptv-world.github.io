8b7963b2dfc1a81e12c3bd84c1f7af6c4e2d9094
iptv-world <107808431+iptv-world@users.noreply.github.com>
2022-07-01 19:56:02 +0600

    Update login-page.js

--- user/dristy/login-page.js
+++ user/dristy/login-page.js
@@ -8,7 +8,7 @@ loginButton.addEventListener("click", (e) => {
     const password = loginForm.password.value;
 
     if (username === "dristy" && password === "tusty") {
-        window.location.href="https://website-iptv-world-live.on.drv.tw/web/user/dristy/";
+        window.location.href="https://iptv-world.github.io/web/dristy/";
     } else {
         loginErrorMsg.style.opacity = 1;
     }
