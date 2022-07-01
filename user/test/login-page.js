7fa1d38128b72d45c0e49392101b27d927363991
iptv-world <107808431+iptv-world@users.noreply.github.com>
2022-07-01 19:56:51 +0600

    Update login-page.js

--- user/test/login-page.js
+++ user/test/login-page.js
@@ -8,7 +8,7 @@ loginButton.addEventListener("click", (e) => {
     const password = loginForm.password.value;
 
     if (username === "tushar" && password === "sysadmin") {
-        window.location.href="https://website-iptv-world-live.on.drv.tw/web/user/test/";
+        window.location.href="https://iptv-world.github.io/web/test/";
     } else {
         loginErrorMsg.style.opacity = 1;
     }
