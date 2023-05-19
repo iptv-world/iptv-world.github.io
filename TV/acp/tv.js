1cc2633781035b77d5551c230eae14265972ed40
iptv-world <107808431+iptv-world@users.noreply.github.com>
2023-05-19 11:43:18 +0600

    Add files via upload

---
+++ TV/acp/tv.js
@@ -0,0 +1,9 @@
+
+	var password; 
+	
+	var cp=2021; 
+	
+	password=prompt('Enter Password',''); 
+	if (password==cp) 
+	alert('Correct Password ! Click OK to Enter !');
+	else window.location.reload(true); 
